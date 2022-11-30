a44cb6e5639410ed782a28e96631ce55659ec9f8
siledth <siledth@gmail.com>
2022-11-30 01:11:09 -0400

    cambios snc 30 1 nov 2022

--- js/dependientes.js
+++ js/dependientes.js
@@ -1,6 +1,7 @@
+
 function llenar_municipio(){
     var id_estado_n = $('#id_estado_n').val();
-   // var base_url = window.location.origin+'/asnc/index.php/evaluacion_desempenio/listar_municipio';
+    //var base_url = window.location.origin+'/asnc/index.php/evaluacion_desempenio/listar_municipio';
     var base_url = '/index.php/evaluacion_desempenio/listar_municipio';
 
     $.ajax({
