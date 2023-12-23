8b9db7e6f9427b59568e494e30a93bff0e17a88f
siledth <siledth@gmail.com>
2023-12-23 19:59:00 -0400

    idtabla5

--- application/views/programacion/nueva_prog.php
+++ application/views/programacion/nueva_prog.php
@@ -74,8 +74,7 @@
                     <div class="col-1"></div>
                     <div class="col-10 mt-3">
                         <h3 class="text-center">Tabla Referente a Proyectos Registrados</h3>
-                        <table id="data-table-default" data-order='[[ 8, "desc" ]]'
-                                class="table table-bordered table-hover">
+                        <table id="table"   class="table table-bordered table-hover">
                             <thead style="background:#e4e7e8">
                                 <tr class="text-center">
                                     <th>Nombre Programación</th>
