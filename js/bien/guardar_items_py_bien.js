5bc982e29387e5e9d7e678647f480ca1437566c7
siledth <siledth@gmail.com>
2023-12-23 22:03:35 -0400

    proyectoobj5

--- js/bien/guardar_items_py_bien.js
+++ js/bien/guardar_items_py_bien.js
@@ -26,21 +26,53 @@ function guardar_py_bien(){
     var estimado_total_t_acc = $("#estimado_total_t_acc").val();
 
 
-    if (par_presupuestaria_acc == '') {
+    if ($("#par_presupuestaria_acc option:selected").val() == 0) {
+        alert("Debe Seleccionar una Partida Presupuestaria");
         document.getElementById("par_presupuestaria_acc").focus();
+        return false;
     }else if(id_estado_acc == ''){
+        alert("Debe ingresar un estado")
         document.getElementById("id_estado_acc").focus();
     }
-    else if(fuente_financiamiento_acc == ''){
-    document.getElementById("fuente_financiamiento_acc").focus();
-    // }else if(porcentaje_acc == ''){
-    //     document.getElementById("porcentaje_acc").focus();
-    // 
+    else if($("#fuente_financiamiento_acc option:selected").val() == 0) {
+        alert("Debe Seleccionar una Fuente Financiamiento");
+        document.getElementById("fuente_financiamiento_acc").focus();
+        return false;
+    }
+    else if(porcentaje_acc == ''){
+        alert("el campo porcentaje no puede quedar vacio")
 
-    }else if(id_ccnu_acc == ''){
+        document.getElementById("porcentaje_acc").focus();
+    }else if($("#id_ccnu_acc option:selected").val() == 0) {
+        alert("Debe Seleccionar un CCNU");
         document.getElementById("id_ccnu_acc").focus();
-    }else if(especificacion_acc == ''){
+        return false;
+    }
+    else if(especificacion_acc == ''){
+        alert("Debe ingresar una especificación")
         document.getElementById("especificacion_acc").focus();
+    }else if($("#id_unidad_medida_acc option:selected").val() == 0) {
+        alert("Debe Seleccionar una unidad de medida");
+        document.getElementById("id_unidad_medida_acc").focus();
+        return false;
+    }
+    else if(cantidad_acc == ''){
+        alert("Debe ingresar una Cantidad (obligatotio)")
+        document.getElementById("cantidad_acc").focus();
+    }
+    // else if(cant_total_distribuir_acc >= '1'){
+    //     alert("la cantidad restante a Distribuir debe ser igual a cero (obligatotio)")
+    //     document.getElementById("cant_total_distribuir_acc").focus();
+    // }
+    else if(costo_unitario_acc == ''){
+        alert("Debe ingresar un Costo Unitario (Obligatorio)")
+
+        document.getElementById("costo_unitario_acc").focus();
+    } else if($("#id_alicuota_iva_acc option:selected").val() == 0) {
+        alert("Debe Seleccionar un iva ");
+        document.getElementById("id_alicuota_iva_acc").focus();
+        return false;
+    
     }else{
         event.preventDefault();
         swal.fire({
