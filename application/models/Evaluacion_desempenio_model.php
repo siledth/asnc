3b06a083ed46226581dc68296b43f57623119a73
siledth <siledth@gmail.com>
2024-01-08 04:29:22 -0400

    evaluaciones_nombre_organoente1

--- application/models/Evaluacion_desempenio_model.php
+++ application/models/Evaluacion_desempenio_model.php
@@ -330,8 +330,8 @@ return $query->result_array();
             $this->db->select('ed.id,
                                 ed.id_usuario,
                                 u.unidad,
-                                concat(tr.desc_rif,\'\',o.rif,\'\',tr2.desc_rif,\'\',e4.rif,\'\',tr3.desc_rif,\'\',ea.rif\'\',en.rif) as rif_organo,
-                                concat(o.desc_organo,\'\',e4.desc_entes,\'\',ea.desc_entes_ads,\'\'en.descripcion) as organo,
+                                en.rif as rif_organo,
+                               en.descripcion as organo,
                                  ed.rif_contrat,
                                  concat(cn.nombre,\'\',c.nombre) as nom_comer,
                                  concat(e2.descedo,\'\', e3.descedo) as est_contratista,
@@ -374,9 +374,6 @@ return $query->result_array();
                                  ed.snc');
             $this->db->join('seguridad.usuarios u', 'u.id = ed.id_usuario');
             $this->db->join('public.organoente en', 'en.codigo = u.unidad', 'left');
-            $this->db->join('public.organos o', 'o.codigo = u.unidad', 'left');
-            $this->db->join('public.entes e4', 'e4.codigo = u.unidad', 'left');
-            $this->db->join('public.entes_ads ea', 'ea.codigo = u.unidad', 'left');
             $this->db->join('public.tipo_rif tr', 'tr.id_rif = o.tipo_rif', 'left');
             $this->db->join('public.tipo_rif tr2', 'tr2.id_rif = e4.tipo_rif', 'left');
             $this->db->join('public.tipo_rif tr3', 'tr3.id_rif = ea.tipo_rif', 'left');
