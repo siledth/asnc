4dbf99daa911b47cd7b8b14f44f5b158baae2deb
siledth <siledth@gmail.com>
2025-05-16 10:39:07 -0400

    dipl 7

--- application/views/diplomado/participantes.php
+++ application/views/diplomado/participantes.php
@@ -39,7 +39,7 @@
 
 
 
-                                            <a onclick="cargarIdInscripcion(<?php echo $data['codigo_planilla'] ?>);"
+                                            <a onclick="cargarIdInscripcion(<?php echo $data['id_inscripcion'] ?>);"
                                                 data-toggle="modal" data-target="#dede" style="color: white">
                                                 <i title="Decisión" class="fas fa-2x fa-fw fa-address-card"
                                                     style="color: darkred;"></i>
